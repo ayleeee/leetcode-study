85bea7c8720f78f4c90baaf5de113a47a67b6e6f
ayleeee <janhyang0309@gmail.com>
2025-04-18 11:04:18 +0900

    solved : number of 1 bits

---
+++ number-of-1-bits/ayleeee.py
@@ -0,0 +1,16 @@
+'''
+n&1 으로 가장 오른쪽 비트를 확인하기
+n = n >>1로 비트를 오른쪽으로 이동시키면서 반복
+
+O(K) => K는 n의 비트 길이. 숫자의 비트 수만큼 반복.
+시간 복잡도는 비트 길이에 비례함
+
+O(1) => 추가 메모리 사용 없이 상수 공간만 사용
+'''
+
+def hammingWeight(self, n: int) -> int:
+    count = 0
+    while n :
+        count +=n & 1
+        n >>= 1
+    return count
